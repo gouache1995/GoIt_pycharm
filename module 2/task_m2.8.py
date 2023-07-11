7ef35cd4972ec2a5847d8c489d116841403a49ae
gouache1995 <67321419+gouache1995@users.noreply.github.com>
2023-07-11 09:26:47 +0300

    task_m2.8

---
+++ module 2/task_m2.8.py
@@ -0,0 +1,6 @@
+message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
+search = "r"
+result = 0
+for search in message:
+    result += 1
+    print(result)
