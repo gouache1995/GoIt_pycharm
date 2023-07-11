f6502e69aab0655a986ef89d875f2422bf12949b
gouache1995 <67321419+gouache1995@users.noreply.github.com>
2023-07-11 09:51:31 +0300

    task_m2.8

--- module 2/task_m2.8.py
+++ module 2/task_m2.8.py
@@ -1,6 +1,7 @@
 message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
 search = "r"
 result = 0
-for search in message:
-    result += 1
-    print(result)
+for i in message:
+    if i == search:
+        result += 1
+        print(result)
